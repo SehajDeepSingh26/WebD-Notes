f07590db7a27bca62056fae3de26b36151bcc623
Sehajdeep Singh <sehajdeep2611@gmail.com>
2023-03-03 21:56:43 +0530

    ..

---
+++ CSS/Project Website/temp.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+int main(){
+        deque<int> d;
+
+        d.push_front(12);
+        d.push_back(7);
+        cout << d.back();
+        // cout>>d.pop_back();
+        
+}
