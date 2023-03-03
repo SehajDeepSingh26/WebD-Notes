4e9fb98f5ab684d54ba24b7f85c705347bc5193d
Sehajdeep Singh <sehajdeep2611@gmail.com>
2023-03-03 22:03:13 +0530

    ..

--- CSS/Project Website/temp.cpp
+++ CSS/Project Website/temp.cpp
@@ -8,7 +8,8 @@ int main(){
 
         d.push_front(12);
         d.push_back(7);
-        cout << d.back();
+        cout << d.back()<<endl;
+        cout << d.front();
         // cout>>d.pop_back();
-        
+        return 0;
 }
